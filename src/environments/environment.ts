47d91b857582ae2ac79fa169125ebd94a865b73f
lanarokip <lankipkoecharo@gmail.com>
2021-08-29 17:35:09 +0300

    adds Api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key: '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'
 };
 
 /*
