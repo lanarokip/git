6cfabdf960b7eda6ee3ebe15f7c7126d824bc722
lanarokip <lankipkoecharo@gmail.com>
2021-08-29 17:28:10 +0300

    adds routing to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GitUsersComponent } from './components/git-users/git-users.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { GithubComponent } from './components/github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component:GitUsersComponent},
+  {path: 'repositories', component:RepoComponent},
+  {path: '', redirectTo:"./components/github/github.component", pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
