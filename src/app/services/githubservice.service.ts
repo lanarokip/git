089c45bd9bb8f088d9f1c958d7ffa1a86efee795
lanarokip <lankipkoecharo@gmail.com>
2021-08-29 17:17:06 +0300

    creates a new service and adds to the service code

---
+++ src/app/services/githubservice.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubserviceService {
+  username:any;
+  repos:any;
+  constructor( private http:HttpClient) { }
+  getUser(user:any){
+    return this.http.get("https://api.github.com/users/" + user+ "?access_token=" + environment.api_key)
+    .pipe(((response:any)=>response));
+  }
+  // users(){
+}
