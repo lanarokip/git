221565eb98f6a1c1402834c280c562a5b4145266
lanarokip <lankipkoecharo@gmail.com>
2021-08-29 17:09:02 +0300

    adds new components repo and git-users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './components/github/github.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { GitUsersComponent } from './components/git-users/git-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    RepoComponent,
+    GitUsersComponent
   ],
   imports: [
     BrowserModule,
