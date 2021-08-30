3bbe8be198d85595fda2915290f9929e71e03270
lanarokip <lankipkoecharo@gmail.com>
2021-08-30 09:34:17 +0300

    imported RepoService  and adder variables users and gituser

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubserviceService } from 'src/app/services/githubservice.service';
+import { RepoService } from 'src/app/services/reposervice.service'
 
 @Component({
   selector: 'app-github',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
-  constructor() { }
+  username:any;
+  gituser:any;
+  gitRepos:any;
+  getUsername(){
+    this.service.getUser(this.username).subscribe(details=>{
+      console.log(details);
+      return this.gituser = details;
+    });
+  }
+  getRepos(){
+    this.aron.getRepo(this.username).subscribe(details=>{
+      console.log(details);
+      return this.gitRepos = details;
+
+    });
+  }
+
+  constructor(private service:GithubserviceService, private aron:RepoService) {
+    
+   }
 
   ngOnInit(): void {
   }
-
 }
